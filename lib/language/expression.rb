b61161ec73b8870af16975c4861030ec7b43a89d
Koki Ryu <liukoki@gmail.com>
2019-06-14 10:43:25 +0900

    expression module

---
+++ lib/language/expression.rb
@@ -0,0 +1,29 @@
+module Expression
+  def free_variables
+    raise NotImplementedError
+  end
+
+  def free_unification_terms
+    raise NotImplementedError
+  end
+
+  def replace(old, new)
+    raise NotImplementedError
+  end
+
+  def occurs(unification_term)
+    raise NotImplementedError
+  end
+
+  def to_s
+    raise NotImplementedError
+  end
+
+  def set_instantiation_time(time)
+    raise NotImplementedError
+  end
+
+  def eql?(term)
+    raise NotImplementedError
+  end
+end
--- lib/language/term.rb
+++ lib/language/term.rb
@@ -1,36 +1,10 @@
+require_relative 'expression.rb'
 class Term
   attr_reader :time, :name
   TIME_DEFAULT = 0
+  include Expression
   def initialize(name)
     @name = name
     @time = TIME_DEFAULT
-  end
-
-  def free_variables
-    raise NotImplementedError
-  end
-
-  def free_unification_terms
-    raise NotImplementedError
-  end
-
-  def replace(old, new)
-    raise NotImplementedError
-  end
-
-  def occurs(unification_term)
-    raise NotImplementedError
-  end
-
-  def to_s
-    raise NotImplementedError
-  end
-
-  def set_instantiation_time(time)
-    raise NotImplementedError
-  end
-
-  def eql?(term)
-    raise NotImplementedError
   end
 end
